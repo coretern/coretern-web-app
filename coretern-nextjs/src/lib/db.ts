80fe996ef0f018f2a815734b029fd2846ca5920b
Anil Kumar <anilarangi6@gmail.com>
2026-04-26 11:37:23 +0530

    change web icon

--- coretern-nextjs/src/lib/db.ts
+++ coretern-nextjs/src/lib/db.ts
@@ -1,26 +1,4 @@
 import mongoose from 'mongoose';
-import dns from 'dns';
-import { Resolver } from 'dns';
-
-// Force ALL DNS resolution through Google Public DNS
-// This fixes ECONNREFUSED on networks that block SRV DNS lookups
-const resolver = new Resolver();
-resolver.setServers(['8.8.8.8', '8.8.4.4', '1.1.1.1']);
-
-// Override the global DNS resolver
-dns.setServers(['8.8.8.8', '8.8.4.4', '1.1.1.1']);
-
-// Also override dns.resolveSrv to use our custom resolver
-const originalResolveSrv = dns.resolveSrv;
-dns.resolveSrv = function (hostname: string, callback: any) {
-    resolver.resolveSrv(hostname, callback);
-} as typeof dns.resolveSrv;
-
-// Override dns.resolveTxt too (used by mongodb+srv)
-const originalResolveTxt = dns.resolveTxt;
-dns.resolveTxt = function (hostname: string, callback: any) {
-    resolver.resolveTxt(hostname, callback);
-} as typeof dns.resolveTxt;
 
 const MONGO_URI = process.env.MONGO_URI;
 
